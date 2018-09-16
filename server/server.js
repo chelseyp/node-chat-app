7cc4d147e9150a8d9f1cc8bd01f1ec385445365c
Chels P <chelsp@Chelss-MacBook-Pro.local>
2018-09-16 16:02:32 -0400

    Implemented broadcasting events

--- server/server.js
+++ server/server.js
@@ -15,21 +15,32 @@ app.use(express.static(publicPath));
 io.on('connection', socket => {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  });
+
   socket.on('createMessage', (msg) => {
     console.log(msg);
 
-    io.emit('newMessage', {
+    // io.emit('newMessage', {
+    //   from: msg.from,
+    //   text: msg.text,
+    //   createdAt: new Date().getTime()
+    // });
+    socket.broadcast.emit('newMessage', {
       from: msg.from,
       text: msg.text,
       createdAt: new Date().getTime()
     });
   });
-
-  // socket.emit('newMessage', {
-  //   from: receivedMsg.from,
-  //   text: receivedMsg.text,
-  //   createdAt: new Date().getTime()
-  // });
   
 
   socket.on('disconnect', () => {
